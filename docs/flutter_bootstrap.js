6252467126554ec684b5fd808a819768cdd909ad
vitolog76-hub <vitolog76@gmail.com>
2026-04-29 22:26:00 +0200

    Deploy automatico - 2026-04-29 22:26:00

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "675882017" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1405645293" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
